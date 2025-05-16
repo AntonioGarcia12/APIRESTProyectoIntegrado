a02c10910748dff384c4561e94943b5dcc1116a7
AntonioGarcia12 <antonio.garcia.harto@gmail.com>
2025-05-16 12:25:40 +0200

    cambio en un repository

--- APICitaMed/src/main/java/com/example/demo/repository/HorarioMedicoRepository.java
+++ APICitaMed/src/main/java/com/example/demo/repository/HorarioMedicoRepository.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.HorarioMedico;
@@ -11,5 +13,12 @@ import com.example.demo.entity.HorarioMedico;
 @Repository("HorarioMedicoRepository")
 public interface HorarioMedicoRepository extends JpaRepository<HorarioMedico, Serializable> {
 
-	List<HorarioMedico> findByMedico_Id(Long idMedico);
+	 @Query("""
+		      SELECT h 
+		        FROM HorarioMedico h
+		   LEFT JOIN Cita c ON c.horario = h
+		       WHERE h.medico.id = :idMedico
+		         AND c.id IS NULL
+		    """)
+	List<HorarioMedico> findByMedico_Id(@Param("idMedico") Long idMedico);
 }
